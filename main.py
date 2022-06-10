96053092f4c6fd7dbc0766206bdb233185548b03
tehelusivepanda <t3helusivepanda@gmail.com>
2022-06-09 23:50:29 -0400

    Hidden token now zz

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import random
 import requests
 import json
 
+import os
+from dotenv import load_dotenv
+
 TOKEN = 'OTg0NTExODg0MzIzNjA2NTc4.GYOPBV.PejGVUEdKxNGl63DlzCDmOqzIFWumx-A1CzvzU'
 dogs = ['https://www.rd.com/wp-content/uploads/2021/01/GettyImages-588935825.jpg',
     'https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/PT_dog_pic_cover_0.png.jpg?itok=wnWCDUk2']
 
 client = discord.Client()
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
 
 def get_quote():
     response = requests.get('https://zenquotes.io/api/random')
