ecb40fdeef241e31e25bf733ab4d02b2b3e4b2a1
tehelusivepanda <t3helusivepanda@gmail.com>
2022-09-09 22:45:27 -0400

    Added Google image scraping functionality

--- jimmy.py
+++ jimmy.py
@@ -3,13 +3,14 @@ import random
 import requests
 import json
 import functions    # Separate py doc with functions
-
 import os
+
 from dotenv import load_dotenv
+from serpapi import GoogleSearch
 
 TOKEN = 'OTg0NTExODg0MzIzNjA2NTc4.GYOPBV.PejGVUEdKxNGl63DlzCDmOqzIFWumx-A1CzvzU'
-dogs = ['https://www.rd.com/wp-content/uploads/2021/01/GettyImages-588935825.jpg',
-    'https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/PT_dog_pic_cover_0.png.jpg?itok=wnWCDUk2']
+# dogs = ['https://www.rd.com/wp-content/uploads/2021/01/GettyImages-588935825.jpg',
+#     'https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/PT_dog_pic_cover_0.png.jpg?itok=wnWCDUk2']
 
 client = discord.Client()
 
@@ -35,7 +36,19 @@ async def on_message(message):
         await message.channel.send(help)
 
     if user_message.startswith('!pic'):
-        await message.channel.send(random.choice(dogs))
+        search_param = user_message.split(" ")[1]
+        search_dict = {"q": search_param,
+            "tbm": "isch",
+            "ijn": "0",
+            "api_key": "ce8ea31a80800c7255235d8d5b2e3a87e475a463f7bd5c6800bdd06a3f23a900"
+        }
+
+        search = GoogleSearch(search_dict)
+        results = search.get_dict()
+        image_results = results["images_results"]
+        #print(image_results)
+        image_ret = [x["original"] for x in image_results]
+        await message.channel.send(random.choice(image_ret))
 
     if user_message.startswith('!quote') or user_message.startswith('!q'):
         quote = functions.get_quote()
